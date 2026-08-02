3fc2a950550b588fc359f1af00af51820060553f
Pranav Thakur <pranavthakur8907@gmail.com>
2026-08-03 01:43:39 +0530

    Added input output basics in C++

---
+++ 01_Basics/02-input-output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    string name;
+cout << "Enter your age:";
+cin >> age;
+cout <<"Enter your name:";
+cin >> name;
+    return 0;
+}
