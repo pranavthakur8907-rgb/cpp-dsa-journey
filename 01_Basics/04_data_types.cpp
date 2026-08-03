bfd6a7258a13c98e4e95e791ce3a3d7a793e5677
Pranav Thakur <pranavthakur8907@gmail.com>
2026-08-04 02:02:00 +0530

    Add data types basics in C++

---
+++ 01_Basics/04_data_types.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age = 20;
+    float height = 1.23;
+    char grade = 'A';
+    double pi = 3.234223322;
+    bool bl = false;
+    string name = "pranav";
+
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Double: " << pi << endl;
+    cout << "Bl: " << bl << endl;
+    return 0;
+}
