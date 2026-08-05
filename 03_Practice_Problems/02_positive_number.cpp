0e37f759c76a2f2be7306d432e0a70f014d0ec90
Pranav Thakur <pranavthakur8907@gmail.com>
2026-08-06 03:53:56 +0530

    Add positive number practice problem

---
+++ 03_Practice_Problems/02_positive_number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int number;
+    cout << " Enter your number " << endl;
+    cin >> number;
+
+    cout << " ===== IF STATEMENT ===== " << endl;
+    if (number > 0) {
+        cout << " Is a postive number " << endl;
+    }
+    return 0;
+}
