323855ffdcf0ecce2bc4f2ee6993685a82e2aea9
Pranav Thakur <pranavthakur8907@gmail.com>
2026-08-08 04:21:17 +0530

    Add switch case statement example

--- 03_Practice_Problems/10_simple_calculator.cpp
+++ 03_Practice_Problems/10_simple_calculator.cpp
@@ -1,41 +1,45 @@
 #include<iostream>
 using namespace std;
-
 int main() {
-    cout << "===== SIMPLE CALCULATOR =====" << endl;
+    cout << " ===== SIMPLE CALCULATOR ===== " << endl;
 
-    int num1, num2;
+    int num1,num2;
     char op;
-
-    cout << "Enter first number: ";
+   
+    cout << " Enter first number " << endl;
     cin >> num1;
 
-    cout << "Enter operator (+, -, *, /): ";
+    cout << " Enter operator (+,-,*,/):" << endl;
     cin >> op;
 
-    cout << "Enter second number: ";
+    cout << " Enter second number " << endl;
     cin >> num2;
 
     if (op == '+') {
-        cout << "Result: " << num1 + num2 << endl;
-    }
-    else if (op == '-') {
-        cout << "Result: " << num1 - num2 << endl;
-    }
-    else if (op == '*') {
-        cout << "Result: " << num1 * num2 << endl;
-    }
-    else if (op == '/') {
-        if (num2 != 0) {
-            cout << "Result: " << num1 / num2 << endl;
-        }
-        else {
-            cout << "Division by zero is not allowed." << endl;
-        }
-    }
-    else {
-        cout << "Invalid operator!" << endl;
+        cout << " Result: " << num1 + num2 << endl;
     }
 
-    return 0;
-}
+    else if (op == '-') {
+        cout << " Result: " << num1 - num2 << endl;
+     }
+
+     else if (op == '*') {
+        cout << " Result: " << num1 * num2 << endl;
+     }
+
+     else if (op == '/') {
+      if (num2 != 0) {
+        cout << " Result: " << num1 / num2 << endl;
+      }
+
+      else {
+        cout << " Division by zero is not allowed. " << endl;
+      }
+    }
+     
+      else {
+        cout << " Invalid operator " << endl;
+      }
+      return 0;
+    }
+
